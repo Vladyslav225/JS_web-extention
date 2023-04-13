8737e9690ffcabf68fb76366532b16d9ef658c31
Yarynovskiy Vladyslav <yvv.work@gmail.com>
2023-04-13 13:26:11 +0300

    - fixed
    - renamed variables for the YouTube mobile and desktop versions
    - created conditions and functions for parsing channel name form YouTube mobile and desktop versions

--- web_extention/script.js
+++ web_extention/script.js
@@ -1,13 +1,20 @@
 function displayResult() {
-    const desktopYouTubeVideoPage = 'https://www.youtube.com/watch?v=';
-    const mobileYouTubeeVideoPage = 'https://m.youtube.com/watch?v='
+    const youTubeeVideoPageMobileVersion = 'https://m.youtube.com/watch?v=';
+    const youTubeVideoPageDesktopVersion = 'https://www.youtube.com/watch?v=';
     const pageGooglePlayApp = 'https://play.google.com/store/apps/details?id=';
 
-    if(window.location.href.includes(desktopYouTubeVideoPage) || window.location.href.includes(mobileYouTubeeVideoPage)) {
-        getYouTubeChanelName((channelName) => {
+    if(window.location.href.includes(youTubeeVideoPageMobileVersion)) {
+        getYouTubeChanelMobileVersion((channelName) => {
             console.log(channelName)
         });
     };
+
+    if(window.location.href.includes(youTubeVideoPageDesktopVersion)) {
+        getYouTubeChanelDesktopVersion((channelName) => {
+            console.log(channelName)
+        });
+    };
+
     if(window.location.href.includes(pageGooglePlayApp)) {
         getGooglePlayPublisher((publisherId) => {
             console.log(publisherId)
@@ -15,29 +22,29 @@ function displayResult() {
     };
 };
 
-function getYouTubeChanelName(callback) {
-    if(window.innerWidth <= 768){
-        const getParentElement = document.getElementsByClassName('slim-owner-icon-and-title')
-        if(getParentElement) {
-            for(i of getParentElement) {
-                const getChannelName = i.getAttribute('href').replace('/', '')
-                callback(getChannelName);
-            };
-        }else {
-            setTimeout(() => {
-                getYouTubeChanelName(callback);
-            }, 1000);
-        }
-    }else {
-        const getParentLinkElement = document.querySelector('yt-formatted-string.style-scope.ytd-channel-name.complex-string');
-        if(getParentLinkElement) {
-            const getChannelName = getParentLinkElement.querySelector('a.yt-simple-endpoint.style-scope.yt-formatted-string').getAttribute('href').replace('/', '');
+function getYouTubeChanelMobileVersion(callback) {
+    const getParentElement = document.getElementsByClassName('slim-owner-icon-and-title')
+    if(getParentElement) {
+        for(i of getParentElement) {
+            const getChannelName = i.getAttribute('href').replace('/', '')
             callback(getChannelName);
-        }else {
-            setTimeout(() => {
-                getYouTubeChanelName(callback);
-            }, 1000);
-        }
+        };
+    }else {
+        setTimeout(() => {
+            getYouTubeChanelMobileVersion(callback);
+        }, 1000);
+    }
+};
+
+function getYouTubeChanelDesktopVersion(callback) {
+    const getParentLinkElement = document.querySelector('yt-formatted-string.style-scope.ytd-channel-name.complex-string');
+    if(getParentLinkElement) {
+        const getChannelName = getParentLinkElement.querySelector('a.yt-simple-endpoint.style-scope.yt-formatted-string').getAttribute('href').replace('/', '');
+        callback(getChannelName);
+    }else {
+        setTimeout(() => {
+            getYouTubeChanelDesktopVersion(callback);
+        }, 1000);
     }
 };
 
